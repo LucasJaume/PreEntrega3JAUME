862555e1f51db5628e52953e2e48a4d2c5aeaff4
LucasJaume <jaumelu@hotmail.com>
2024-02-05 19:41:17 -0300

    Modifico y finalizo con lo pedido en la pre entrega 3!

--- js/compra.js
+++ js/compra.js
@@ -1,10 +1,31 @@
+const tablebody=document.querySelector("tbody")
+const totalPrecioElement = document.getElementById("total-precio");
+
 function recuperarCarrito() {
     return JSON.parse(localStorage.getItem("miCarrito"))
 }
-const carritoNuevo=recuperarCarrito()
+const carritoNuevo=recuperarCarrito() 
 
-if(carritoNuevo){
-    carritoNuevo.forEach((producto)=>{
-        console.log(producto)
-    })
+function armarFilaHtml(producto){
+    return`<tr>
+                <td>${producto.imagen}</td>
+                <td>${producto.nombre}</td>
+                <td>$ ${producto.precio}</td>
+                <td data-eliminar="" class="boton-Eliminar">x</td>
+            </tr>`
 }
+
+function mostrarCarrito() {
+    tablebody.innerHTML = "";
+    let totalPrecio = 0;
+
+    carritoNuevo.forEach((producto) => {
+        tablebody.innerHTML += armarFilaHtml(producto);
+        totalPrecio += producto.precio;
+    });
+
+    // Mostrar el total en el pie de la tabla
+    totalPrecioElement.textContent = `$ ${totalPrecio.toFixed(2)}`;
+}
+
+mostrarCarrito();
